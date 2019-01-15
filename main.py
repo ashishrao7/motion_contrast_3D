1f57e04333f1dd81483e80a68150f19fb292ff7a
IISc_Ashish <ashish.rao.m@gmail.com>
2019-01-15 10:50:28 +0530

    added colorbar

--- main.py
+++ main.py
@@ -18,14 +18,14 @@ def compute_depth(event, depth_map, baseline, focal_length, scan_speed, start_ti
   disparity = x - (time-start_time)*scan_speed
   if disparity != 0:
     depth = (baseline*focal_length) / disparity
-  
+
   else:
-    depth = 10000     #data is captured for a horizontal line hence x
+    depth = 100000     #data is captured for a horizontal line hence x
   #print(y, x, depth)
   depth_map[int(x)][int(y)].append(depth)
 
 def compute_final_depth_map(depth_map):
-  depth_map_new = [[sum(depth_map[i][j])/ len(depth_map[i][j]) if len(depth_map[i][j])!= 0 else 10000 for j in range(346)] for i in range(260)]
+  depth_map_new = [[sum(depth_map[i][j])/ len(depth_map[i][j]) if len(depth_map[i][j])!= 0 else 100000 for j in range(346)] for i in range(260)]
   depth_map_mat = np.array(depth_map_new)
   return depth_map_mat
 
@@ -40,6 +40,7 @@ def plot_depth_map(depth_map):
   image /= image.max()/255.0 
   image = ndimage.rotate(image, 180) 
   plt.imshow(image)
+  plt.colorbar()
   plt.show()
 
 def convert_to_pcd_and_store(depth_map_matrix):
@@ -47,9 +48,9 @@ def convert_to_pcd_and_store(depth_map_matrix):
 
 def main():
   camera_dims = (260, 346)
-  focal_length = 538 #along y-direction
+  focal_length = 538 # along y-direction
   baseline = 0.015
-  scan_speed = 62.5 #pixels per second coverge
+  scan_speed = 60 # pixels per second coverge
   
   depth_map = init_depth_map(camera_dims)
   
