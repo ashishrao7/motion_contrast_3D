31986501a9a534a499482b7dd806b54a7cc80ba3
IISc_Ashish <ashish.rao.m@gmail.com>
2019-01-30 14:20:25 +0530

    applied nathan's disparity formula

--- main.py
+++ main.py
@@ -4,10 +4,10 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 from scipy import ndimage
-from mpl_toolkits.mplot3d import Axes3D
 
-bad_depth = 5 #value set to areas which have 0 disparity or areas for which no disparity has been recorded
-proj_focal_length = 150
+
+bad_depth = 100000 #value set to areas which have 0 disparity or areas for which no disparity has been recorded
+
 def init_depth_map(camera_dims):
   ''' 
     Function to define the intial size of the depth map matrix. Based on the size of the camera dimensions.
@@ -26,30 +26,24 @@ def read_data(path):
   return events, start_time
 
 
-def compute_depth(event, depth_map, baseline, focal_length, scan_speed, start_time, R, t):
+def compute_depth(event, depth_map, scan_speed, start_time, focal_length, baseline):
   '''
     Compute depth for each pixel in the depth map. Some pixels could have multiple depths due to multiple activations on the event camera. All the possible values are stored as a list in the 
     corresponding pixel loction. The formula used to calculate depth is the one described in Matsuda et al.
   '''
   
   time, y, x, _ = event #x and y are interchanged because the convention followed is different
-  # Do the rotation translation on point x y and compute disparity
-  im_coord = np.dot(R.T, np.array(([x] ,[y],[focal_length])) - 1.36*t) + 1.36*t 
 
-  new_x = proj_focal_length * im_coord[0]/(im_coord[2])
-  new_y = proj_focal_length * im_coord[1]/(im_coord[2])
-  disparity = new_x - (time-start_time)*scan_speed #data is captured for a horizontal line hence x is used as opposed to y
+  disparity = x - 246*((time-start_time)%(1/scan_speed))*scan_speed #data is captured for a horizontal line hence x is used as opposed to y
 
   if disparity != 0:
-    depth = focal_length * baseline/-disparity
+    depth = focal_length * baseline / disparity
   else:
     depth = bad_depth   
 
   # print("Time Elapsed:{} Disparity :{} Depth:{}".format(time-start_time, disparity, depth))
   # print("new_x:{} new_y:{}".format(new_x, new_y))
-  if new_x < 260:
-    if new_y < 346:
-      depth_map[int(x)][int(y)].append(depth)
+  depth_map[int(x)][int(y)].append(depth)
 
 def compute_final_depth_map(depth_map):
   '''
@@ -71,57 +65,29 @@ def plot_depth_map(depth_map):
   plt.xlabel('Camera x co-ord')
   plt.xlim(0, 345)
   plt.ylim(0, 259)
-  image = (depth_map < 800)*depth_map
-  image = (image > 0)*image
-  image = (image == 0) * 800 + image
-  image = ndimage.rotate(image, 180) 
-  plt.imshow(image, cmap ='gray')
+  image = ndimage.rotate(depth_map, 180) 
+  plt.imshow(image, cmap ='rainbow')
   plt.colorbar()
   plt.show()
-
-
-'''def plot_3D_space(depth_mat):
-  volume_vector_list = []
-  for i in range(260):
-    for j in range(346):
-      if depth_mat[i][j] < 0:
-        volume_vector_list.append(list((i, j, depth_mat[i][j])))
-  fig = plt.figure()
-  ax = fig.add_subplot(111, projection='3d')
-  xs = np.array(volume_vector_list)[:, 0]
-  ys = np.array(volume_vector_list)[:, 1]
-  zs = np.array(volume_vector_list)[:, 2]
-  ax.scatter(xs, zs, ys)
-  plt.show()'''
-
 
 def convert_to_pcd_and_store(depth_map_matrix):
   pass 
 
 def main():
 
-  camera_dims = (260,346)  # Dimensions of a DAVIS346
-  focal_length = 450 # along y-direction
-    # 15 cm baseline was used in the experiment
-  scan_speed = 60     # pixels per second coverge , determined by pattern generatad in generator.py
-  
-  R = np.array([[0.9910483601989318, -0.001257937490197782, -0.1334974357067545],
-                [-0.05010500313601597, 0.9233539397380011, -0.3806665084178691 ],
-                [0.1237442378769538, 0.3839478083849191, 0.9150253788986781]])
-  
-  t = np.array([[2.100388684052633],
-               [-54.19534011618826],
-               [-35.31909568783666]])
+  offset = 1
 
-  baseline = np.linalg.norm(t)
-  print('The baseline is {}'.format(baseline))
+  camera_dims = (260,346)  # Dimensions of a DAVIS346
+
+  scan_speed = 60     # pixels per second coverge , determined by pattern generatad in generator.py
 
   depth_map = init_depth_map(camera_dims)
   
-  events, start_time = read_data('bear_exp_1/events.txt')
+  events,start_time = read_data('Experiment-1/events copy.txt')
+  focal_length, baseline = 450, 15
   
   for event in events:
-    compute_depth(event, depth_map, baseline, focal_length, scan_speed, start_time, R,t)
+    compute_depth(event, depth_map, scan_speed, start_time+offset, focal_length, baseline)
 
   depth_map_matrix = compute_final_depth_map(depth_map)
 
